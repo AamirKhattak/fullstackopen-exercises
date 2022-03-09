fe655e7fa883d02297236310defbffa9e1256771
mrgreen_pk <aamir.fui@gmail.com>
2022-03-09 16:09:37 +0500

    ex till 2.18 completed

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import phonebookService from "./services/phonebook";
 
 const Filter = ({ filterText, handleOnChangeFilter }) => {
   return (
@@ -30,12 +31,13 @@ const PersonForm = ({
   );
 };
 
-const Persons = ({ persons }) => {
+const Persons = ({ persons, onDelete }) => {
   return (
     <div>
       {persons.map((person) => (
         <p key={person.id}>
-          {person.name} {person.number}
+          {person.name} {person.number}{" "}
+          <button onClick={() => onDelete(person.id)}> delete </button>
         </p>
       ))}
     </div>
@@ -43,12 +45,15 @@ const Persons = ({ persons }) => {
 };
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+
+  useEffect(() => {
+    phonebookService.getAll().then((initialData) => {
+      setPersons(initialData);
+      console.log(initialData);
+    });
+  }, []);
+
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
   const [filter, setFilter] = useState("");
@@ -57,25 +62,81 @@ const App = () => {
   const handleOnChangeNumber = (event) => setNewNumber(event.target.value);
   const handleOnChangeFilter = (event) => setFilter(event.target.value);
 
-  const filteredPersons = persons.filter((per) =>
-    per.name.toLowerCase().includes(filter.toLowerCase())
-  );
+  const onDelete = (id) => {
+    let tobeDeletedPerson = persons.find((per) => per.id === id)[0];
+    // console.log(tobeDeletedPerson);
+
+    if (!window.confirm(`Delete ${tobeDeletedPerson.name}?`)) {
+      return;
+    }
+
+    phonebookService
+      .remove(id)
+      .then((response) => {
+        setPersons(persons.filter((per) => per.id !== id));
+        // setNotification({message:`${deletedPerson[0].name} has been deleted`,});
+        // setTimeout(() => {setNotification(undefined)}, 5000);
+      })
+      .catch((error) => {
+        setPersons(persons.filter((per) => per.id !== id));
+        console.log(tobeDeletedPerson);
+        // setNotification({message:`${deletedPerson[0].name} has been already deleted`,});
+        // setTimeout(() => {setNotification(undefined)}, 5000);
+      });
+  };
 
   const handleOnSubmit = (event) => {
     event.preventDefault();
     const alreadyExists = persons.find((per) => per.name === newName);
     if (alreadyExists) {
-      alert(`${newName} is already added to the phonebook`);
+      if(window.confirm(`${newName} is already added to phonebook, replace the old number with the new one.`)){
+        const updatedPerson = {...alreadyExists, number: newNumber}
+        updatePerson(updatedPerson)
+      }
       return;
     }
-    setPersons(
-      persons.concat({
-        name: newName,
-        number: newNumber,
-        id: persons.length + 1,
-      })
-    );
+
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+
+    savePerson(newPerson);
   };
+
+  const updatePerson = (updatedPerson) => {
+    console.log('isAlreadyExists',updatedPerson);
+    phonebookService.update(updatedPerson.id, {...updatedPerson})
+    .then( returnedPerson => {
+        setPersons(
+            persons.map( per => per.id === updatedPerson.id ? returnedPerson : per)
+        )
+        
+        // setNotification({message:'Updated ' + returnedPerson.name, type:'success'})
+        // setTimeout(() => {setNotification(undefined)}, 5000);
+    }).catch(error => console.log(error))
+  }
+  const savePerson = (newPerson) => {
+    phonebookService
+      .create(newPerson)
+      .then((returnedPerson) => {
+        setPersons(persons.concat(returnedPerson));
+        // setNotification({message:'Added ' + returnedPerson.name, type:'success'})
+        // setTimeout(() => {setNotification(undefined)}, 5000);
+        setNewName("");
+        setNewNumber("");
+      })
+      .catch((error) => {
+        console.error(error.response.data);
+        // setNotification({message: error.response.data.error});
+        // setTimeout(() => {setNotification(undefined)}, 5000);
+      });
+  };
+
+  const filteredPersons = persons.filter((per) =>
+    per.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
     <div>
@@ -90,7 +151,7 @@ const App = () => {
         onSubmit={handleOnSubmit}
       />
       <h2>Numbers</h2>
-      <Persons persons={filteredPersons} />
+      <Persons persons={filteredPersons} onDelete={onDelete} />
     </div>
   );
 };
