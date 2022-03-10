b5c220ab01170e2e5d44d06a2f26d1cfaa35bbbd
mrgreen_pk <aamir.fui@gmail.com>
2022-03-10 14:23:39 +0500

    til;l 3.4 cmoplte

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,45 +2,61 @@ const express = require("express");
 const app = express();
 
 let phonebook = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
+    {
+        id: 1,
+        name: "Arto Hellas",
+        number: "040-123456"
+    }, {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    }, {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    }, {
+        id: 4,
+        name: "Mary Poppendieck",
+        number: "39-23-6423122"
+    }
 ];
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!</h1>");
+    res.send("<h1>Hello World!</h1>");
 });
 
 app.get("/info", (req, res) => {
-  res.send(
-    `Phonebook has info for ${phonebook.length} people <br/><br/> ${new Date()}`
-  );
+    res.send(
+        `Phonebook has info for ${phonebook.length} people <br/><br/> ${new Date()}`
+    );
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(phonebook);
+    res.json(phonebook);
 });
+
+app.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const personFound = phonebook.find((per) => per.id === id);
+    if (!personFound) {
+        res
+            .status(404)
+            .end();
+        return;
+    }
+    res.json(personFound);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    phonebook = phonebook.filter(per => per.id !== id);
+    res.status(204).end()
+  })
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
