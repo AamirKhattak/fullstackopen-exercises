9f3d08d99bf6a087fa6d822444a17c3966fbfe93
mrgreen_pk <aamir.fui@gmail.com>
2022-03-11 11:06:37 +0500

    part setup

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -2,90 +2,74 @@ const express = require("express");
 const app = express();
 
 let phonebook = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    }, {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    }, {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    }, {
-        id: 4,
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
 ];
 
 app.use(express.json());
 
-const generateId = () => Math.floor(Math.random() * 100000)
+const generateId = () => Math.floor(Math.random() * 100000);
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello World!</h1>");
+  res.send("<h1>Hello World!</h1>");
 });
 
 app.get("/info", (req, res) => {
-    res.send(
-        `Phonebook has info for ${phonebook.length} people <br/><br/> ${new Date()}`
-    );
+  res.send(
+    `Phonebook has info for ${phonebook.length} people <br/><br/> ${new Date()}`
+  );
 });
 
 app.get("/api/persons", (req, res) => {
-    res.json(phonebook);
+  res.json(phonebook);
 });
 
 app.get("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
-    const personFound = phonebook.find((per) => per.id === id);
-    if (!personFound) {
-        res
-            .status(404)
-            .end();
-        return;
-    }
-    res.json(personFound);
+  const id = Number(req.params.id);
+  const personFound = phonebook.find((per) => per.id === id);
+  if (!personFound) return res.status(404).end();
+
+  res.json(personFound);
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-    const id = Number(req.params.id);
-    phonebook = phonebook.filter((per) => per.id !== id);
-    res
-        .status(204)
-        .end();
+  const id = Number(req.params.id);
+  phonebook = phonebook.filter((per) => per.id !== id);
+  res.status(204).end();
 });
 
-app.post("/api/persons", (req, res)=>{
+app.post("/api/persons", (req, res) => {
+  const body = req.body;
+  if (!(body.name && body.number)) {
+    return res.status(400).json({ error: "name or number is missing" });
+  } else if (phonebook.find((per) => per.name === body.name)) {
+    return res.status(400).json({ error: "name already exists in the phonebook" });
+  }
 
-    const body = req.body;
-    console.log(req);
-    
-
-    if(!(body.name || body.number)){
-        console.log(body);
-        
-        return res.status(400).json({ 
-            error: 'content missing' 
-          })
-    }
-
-    const newPerson = {
-        name: body.name,
-        number: body.number,
-        id: generateId()
-    }
-
-    phonebook = phonebook.concat(newPerson);
-
-    res.json(newPerson);
-
+  const newPerson = { name: body.name, number: body.number, id: generateId() };
+  phonebook = phonebook.concat(newPerson);
+  res.json(newPerson);
 });
 
 const PORT = 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
