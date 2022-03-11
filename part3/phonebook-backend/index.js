744a27c38451c02be4ae2b30d4d50270fcc27232
mrgreen_pk <aamir.fui@gmail.com>
2022-03-11 11:52:19 +0500

    till 3.8 morgan config complete, code refectored

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const app = express();
+var morgan = require("morgan");
 
-var morgan = require("morgan")
+const app = express();
 
 let phonebook = [
   {
@@ -26,27 +26,31 @@ let phonebook = [
   },
 ];
 
-
-// const requestLogger = (request, response, next) => {
-//   console.log('Method:', request.method)
-//   console.log('Path:  ', request.path)
-//   console.log('Body:  ', request.body)
-//   console.log('res Body:  ', response.da)
-//   console.log('---')
-//   next()
-// }
-
 app.use(express.json());
-// app.use(morgan('tiny'))
-morgan.token('body', (req, res) => {
+
+//###########################################################<start> MORGAN LOGGING
+//using tiny config, for all methods except POST
+app.use(
+  morgan("tiny", {
+    skip: (req, res) => req.method === "POST",
+  })
+);
+
+morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
-})
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+});
+//only to log POST requests
+app.use(
+  morgan(
+    ":method :url :status :res[content-length] - :response-time ms :body",
+    {
+      skip: (req, res) => req.method !== "POST",
+    }
+  )
+);
+//###########################################################<end> MORGAN LOGGING
 
-// app.use(requestLogger)
-
-
-
+//###########################################################<start> API CALLS
 const generateId = () => Math.floor(Math.random() * 100000);
 
 app.get("/", (req, res) => {
@@ -82,19 +86,27 @@ app.post("/api/persons", (req, res) => {
   if (!(body.name && body.number)) {
     return res.status(400).json({ error: "name or number is missing" });
   } else if (phonebook.find((per) => per.name === body.name)) {
-    return res.status(400).json({ error: "name already exists in the phonebook" });
+    return res
+      .status(400)
+      .json({ error: "name already exists in the phonebook" });
   }
 
-  const newPerson = { name: body.name, number: body.number, id: generateId() };
+  const newPerson = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  };
   phonebook = phonebook.concat(newPerson);
   res.json(newPerson);
 });
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
-app.use(unknownEndpoint)
+app.use(unknownEndpoint);
+//###########################################################<end> API CALLS
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
