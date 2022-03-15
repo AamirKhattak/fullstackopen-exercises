b0f0e6a9e850250e01c225a5d7e4354f70f56a04
mrgreen_pk <aamir.fui@gmail.com>
2022-03-15 22:54:41 +0500

    CONFIG

--- part4/bloglist-backend/utils/list_helper.js
+++ part4/bloglist-backend/utils/list_helper.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash')
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -20,8 +22,32 @@ const favoriteBlog = (blogs) => {
   return { title: favBlog.title, author: favBlog.author, likes: favBlog.likes };
 };
 
+const mostBlogs = (blogs) => {
+  let blogsGroupBy = lodash.groupBy(blogs, 'author');
+  let authorBlogsSum = []; // name of author with total likes of his/her blogs
+  lodash.forEach(blogsGroupBy, (value, key) => {
+      authorBlogsSum.push({'author': key, 'blogs': value.length   });
+  })
+  let authorWithMostBlogs =  authorBlogsSum.reduce( (p, c) => p.blogs > c.blogs ? p : c);
+  return {author:authorWithMostBlogs.author, blogs: authorWithMostBlogs.blogs};
+}
+
+const mostLikes = (blogs) =>{
+  let blogsGroupBy = lodash.groupBy(blogs, 'author');
+  let authorLikesSum = []; // name of author with total likes of his/her blogs
+  lodash.forEach(blogsGroupBy, (value, key) => {
+      authorLikesSum.push({'author': key, 'likes':lodash.sumBy(blogsGroupBy[key],'likes')});
+  }) 
+  let authorWithMostLikes = authorLikesSum.reduce((p,c) => {
+      return p.likes > c.likes ? p : c
+  })
+  return {author:authorWithMostLikes.author, likes: authorWithMostLikes.likes};
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes
 };
