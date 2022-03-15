ba44492a199e323aa70799098fda9cc816378123
mrgreen_pk <aamir.fui@gmail.com>
2022-03-15 09:50:15 +0500

    link to backend repo is update - phonebook

--- part2/phonebook/src/services/phonebook.js
+++ part2/phonebook/src/services/phonebook.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const baseUrl = "/api/persons";
-// const baseUrl = "https://safe-lake-59059.herokuapp.com/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
--- part3/phonebook-backend/README.md
+++ part3/phonebook-backend/README.md
@@ -1,2 +1,4 @@
 ## Link to backend Heroku app
 https://safe-lake-59059.herokuapp.com
+
+Backend repo is at https://github.com/AamirKhattak/phonebook-backend-heroku
