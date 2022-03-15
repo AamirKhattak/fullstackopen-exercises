abe6c2285df6f06a8c406099e452b4d99aaaf14d
mrgreen_pk <aamir.fui@gmail.com>
2022-03-16 01:22:21 +0500

    4.9 complete

--- part4/bloglist-backend/tests/blog_api.test.js
+++ part4/bloglist-backend/tests/blog_api.test.js
@@ -2,27 +2,35 @@ const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 
-
 const api = supertest(app);
 
-const Blog = require("../models/blog")
-const helper = require("./test_helper_blogs")
+const Blog = require("../models/blog");
+const helper = require("./test_helper_blogs");
 
 beforeEach(async () => {
-  await Blog.deleteMany({})
-  await Blog.insertMany(helper.initialBlogs)
-})
-
+    await Blog.deleteMany({});
+    await Blog.insertMany(helper.initialBlogs);
+});
 
 describe("blog-api", () => {
-  test("blogs are returned as json", async () => {
-    await api
-      .get("/api/blogs")
-      .expect(200)
-      .expect("Content-Type", /application\/json/);
-  }, 10000);
+    test("returns correct amount of blog posts in JSON format", async () => {
+        const response = await api
+            .get("/api/blogs")
+            .expect(200)
+            .expect("Content-Type", /application\/json/);
+
+        expect(response.body).toHaveLength(helper.initialBlogs.length);
+    }, 10000);
+
+    test("the unique identifier property of the blog posts is named id", async () => {
+      const response = await api.get("/api/blogs");
+      console.log(response);
+      response.body.map( blog => expect(blog.id).toBeDefined())
+    })
 });
 
 afterAll(() => {
-  mongoose.connection.close();
+    mongoose
+        .connection
+        .close();
 });
