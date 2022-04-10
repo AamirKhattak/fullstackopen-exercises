a3fb803d454e4f938460b6bc0b90e564f4aad6d8
mrgreen_pk <aamir.fui@gmail.com>
2022-04-10 23:52:22 +0500

    till 6.5 completed, order by number of votes

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,45 @@
+import { useSelector, useDispatch } from "react-redux";
+
+import { createAnecdote, voteAnecdote } from "./reducers/anecdoteReducer";
+
+const App = () => {
+  const anecdotes = useSelector((state) => state.sort((a, b) => b.votes - a.votes));
+  const dispatch = useDispatch();
+
+  const vote = (id) => {
+    console.log("vote", id);
+    dispatch(voteAnecdote(id));
+  };
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+    console.log("handleOnSubmit");
+    const anecdote = event.target.anecdote.value;
+    event.target.anecdote.value = "";
+    dispatch(createAnecdote(anecdote));
+  };
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+      <h2>create new</h2>
+      <form onSubmit={handleOnSubmit}>
+        <div>
+          <input name="anecdote" />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  );
+};
+
+export default App;
